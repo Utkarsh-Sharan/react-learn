894e1209b8a3a55249a8931adf6c71016b76212b
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-09-22 11:52:13 +0530

    run react

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,5 +1,13 @@
+import Youtube from "./Youtube";
+
 function App() {
-  return <h1>Classic react app</h1>;
+  return (
+    //we wrap multiple lines of html in this fragment-> <> </>
+    <>
+      <h1>Classic react app</h1>
+      <Youtube />
+    </>
+  );
 }
 
 export default App;

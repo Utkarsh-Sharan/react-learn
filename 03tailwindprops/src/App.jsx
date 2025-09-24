61328f4ad79ae9fffadc6e738cedd907dd0b8533
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-09-24 11:09:44 +0530

    create card with tailwind and vite

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className="text-3xl bg-green-500 p-3 rounded-md">
         Vite with tailwind
       </h1>
+
+      <Card userName="Utkarsh Sharan" userPost="Game Developer, India" />
+      <Card userName="Rohan Sharan" userPost="Web Developer, India" />
+      <Card />
     </>
   );
 }
